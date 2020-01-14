8af09fc6aabeefdf8d7a471540dc96e891819263
RedFu01 <konradfuger@googlemail.com>
2020-01-14 11:23:31 +0100

    verifying

--- src/main.c
+++ src/main.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdbool.h>
-#include <math.h>
 
 double target_x = 100.0;
 double target_y = 2.0;
@@ -18,13 +17,15 @@ bool is_out_of_bounds();
 void rotate();
 void move();
 void reset();
+double pow2(double a);
 
-/*@ ensures needle_x - target_x <= target_radius;
-  @ ensures needle_y - target_y <= target_radius;
-  @ assigns needle_x, needle_y;
+/*@ ensures (needle_x - target_x) * (needle_x - target_x) + (needle_y - target_y) + (needle_y - target_y) <= target_radius * target_radius;
+  @ assigns needle_x, needle_y, v_y;
   @*/
 int main(int argc, char *argv[])
 {
+	/*@ loop invariant (needle_x - target_x) * (needle_x - target_x) + (needle_y - target_y) + (needle_y - target_y) > target_radius * target_radius;
+ 	  @*/
 	while (!is_in_target_region())
 	{
 		move();
@@ -45,17 +46,37 @@ int main(int argc, char *argv[])
 	return 0;
 }
 
+/*@ assigns \nothing;
+  @ behavior positive:
+  @	  assumes (needle_x - target_x) * (needle_x - target_x) + (needle_y - target_y) * (needle_y - target_y) <= target_radius * target_radius;
+  @	  ensures \result == true;
+  @
+  @ behavior negative:
+  @	  assumes (needle_x - target_x) * (needle_x - target_x) + (needle_y - target_y) * (needle_y - target_y) > target_radius * target_radius;
+  @	  ensures \result == false;
+  @*/
 bool is_in_target_region()
 {
-	return pow(needle_x - target_x, 2) + pow(needle_y - target_y, 2) <= target_radius;
+	return pow2(needle_x - target_x) + pow2(needle_y - target_y) <= pow2(target_radius);
 }
 
+/*@ assigns \nothing;
+  @ behavior positive:
+  @	  assumes (needle_y - target_y) * (needle_y - target_y) <= target_radius;
+  @	  ensures \result == true;
+  @
+  @ behavior negative:
+  @	  assumes (needle_y - target_y) * (needle_y - target_y) > target_radius;
+  @	  ensures \result == false;
+  @*/
 bool is_in_safe_corridor()
 {
-	printf("%f, %f\n", pow(needle_y - target_y, 2), pow(target_radius,2));
-	return pow(needle_y - target_y, 2) <= pow(target_radius,2);
+	return pow2(needle_y - target_y) <= pow2(target_radius);
 }
 
+/*@ assigns \nothing;
+  @ ensures \result == false; 
+*/
 bool is_out_of_bounds() {
 	return false;
 }
@@ -66,7 +87,6 @@ bool is_out_of_bounds() {
 void rotate()
 {
 	v_y *= -1;
-	printf("ROTATE \n");
 }
 
 /*@ ensures \old(needle_x) == needle_x + v_x;
@@ -77,10 +97,22 @@ void move()
 {
 	needle_x += v_x;
 	needle_y += v_y;
-	printf("MOVE \n");
 }
 
+/*@ assigns needle_x, needle_y;
+  @ ensures needle_x == 0; 
+  @ ensures needle_y == 0; 
+*/
 void reset()
 {
-		printf("RESET \n");
+	needle_x = 0.0;
+	needle_y = 0.0;
+}
+
+
+/*@ ensures \result == a*a;
+  @ assigns \nothing;
+*/
+double pow2(double a) {
+	return a*a;
 }
---
+++ verify_debug.bash
@@ -0,0 +1,1 @@
+frama-c-gui -slevel 10 -warn-decimal-float 0 -val ./src/main.c
